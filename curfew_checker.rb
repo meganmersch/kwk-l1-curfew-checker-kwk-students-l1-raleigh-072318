b1d2ea96b966bb493483747cb735d5937dbc42cc
meganmersch <meganrm8@gmail.com>
2018-07-25 13:13:21 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,10 +1,8 @@
 
 def platinum_curfew_checker(n)
-  if 
-    n >= 11
+  if n >= 11
     puts "You're in trouble! Better get back to Hogwarts quick!"
-  elsif 
-    n < 11
+  elsif n < 11
     puts "you have #{n - 11} hour(s) left to keep having fun!"
   end
 end
